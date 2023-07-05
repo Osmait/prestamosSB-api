811bf7174bfd79c800f23fd065c2e9840e904198
osmait <saulburgos6@gmail.com>
2023-07-05 14:58:37 -0400

    add User Domain

---
+++ src/main/java/com/prestamossb/prestamossbapi/domain/user/User.java
@@ -0,0 +1,41 @@
+package com.prestamossb.prestamossbapi.domain.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    @Column(name = "last_name0",nullable = false)
+    private String lastName;
+    private String phone;
+    @Column(nullable = false)
+    private  String address;
+    @Column(nullable = false)
+    private  String email;
+    @Column(name = "deleted",columnDefinition = "boolean default false")
+    private  boolean deleted;
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createAt;
+    @Column(name = "update_at")
+    @UpdateTimestamp
+    private LocalDateTime UpdateAt;
+
+
+}
