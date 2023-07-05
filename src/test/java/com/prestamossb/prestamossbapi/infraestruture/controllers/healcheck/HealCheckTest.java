b81a670176e1f2da9a839d00fc4cd0a86425fa6a
Saul Burgos <saulburgos6@gmail.com>
2023-07-05 15:50:18 -0400

    fix test healh-check test

--- src/test/java/com/prestamossb/prestamossbapi/infraestruture/controllers/healcheck/HealCheckTest.java
+++ src/test/java/com/prestamossb/prestamossbapi/infraestruture/controllers/healcheck/HealCheckTest.java
@@ -1,22 +1,37 @@
 package com.prestamossb.prestamossbapi.infraestruture.controllers.healcheck;
 
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Testcontainers
 class HealCheckTest {
 
     @Autowired
     private MockMvc mockMvc;
-
+    @Container
+    public static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:9.6.12");
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dymDynamicPropertyRegistry) {
+        dymDynamicPropertyRegistry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        dymDynamicPropertyRegistry.add("spring.datasource.username",postgresContainer::getUsername);
+        dymDynamicPropertyRegistry.add("spring.datasource.password",postgresContainer::getPassword);
+    }
     @Test
     void healCheck() throws Exception {
         mockMvc.perform(get("/health-check"))
